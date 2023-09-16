6c6ce996038c4ef277ffea03a9f33cb9a5927eb8
Jose <jlmg.96.jm@gmail.com>
2023-09-16 14:36:26 -0300

    remove: comments in app.ts

--- src/app.ts
+++ src/app.ts
@@ -11,15 +11,12 @@ import { envs } from "./config";
 
 
 async function main() {
-    //todo: await base de datos
 
     await MongoDatabase.connect({
         dbName: envs.MONGO_DB_NAME,
         mongoUrl: envs.MONGO_URL,
     })
 
-    //todo: inicio de nuestro server
-    // new Server( { port: 4000 } ).start();
     new Server({ 
         port: Number(envs.NODE_PORT),
         routes: AppRoutes.routes,
