9520b01c6bb438d08291d39da1627884f075c570
Jose <jlmg.96.jm@gmail.com>
2023-09-16 15:31:14 -0300

    Add: bcrypt, encrypt the password

--- src/infraestructure/datasources/auth.datasource.impl.ts
+++ src/infraestructure/datasources/auth.datasource.impl.ts
@@ -1,5 +1,6 @@
 import { UserModel } from "../../data/mongodb";
 import { AuthDatasource, CustomError, RegisterUserDto, UserEntity } from "../../domain";
+import { BcryptAdapter } from "../../config";
 
 
 // acá se implementa la base de datos
@@ -18,12 +19,13 @@ export class AuthDataSourceImpl implements AuthDatasource {
             const user = await UserModel.create({
                 name: name,
                 email: email,
-                password: password,
+                password: BcryptAdapter.hash(password),
             });
 
             await user.save();
             
             //2. Hash de contraseña
+
 
 
             //3. Mapear la respuesta a nuestra entity
@@ -32,7 +34,7 @@ export class AuthDataSourceImpl implements AuthDatasource {
                 user.id,
                 name,
                 email,
-                password,
+                user.password,
                 user.roles,
             );
             
