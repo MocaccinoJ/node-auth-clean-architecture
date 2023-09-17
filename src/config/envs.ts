e3d4f5c80ccd0ae6e7139eafcb4c816c15ba71f8
Jose <jlmg.96.jm@gmail.com>
2023-09-17 18:46:09 -0300

    Add: JWT secret env

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -6,4 +6,5 @@ export const envs = {
     NODE_PORT: get('NODE_PORT').required().asPortNumber(),
     MONGO_URL: get('MONGO_URL').required().asString(),
     MONGO_DB_NAME: get('MONGO_DB_NAME').required().asString(),
+    JWT_SEED: get('JWT_SEED').required().asString(),
 }
--- src/config/jwt.ts
+++ src/config/jwt.ts
@@ -1,6 +1,7 @@
 import jwt from 'jsonwebtoken';
+import { envs } from './index';
 
-
+const JWT_SEED = envs.JWT_SEED;
 
 export class JtwAdappter {
 
@@ -8,7 +9,7 @@ export class JtwAdappter {
         
         return new Promise( ( resolve ) => {
 
-            jwt.sign(payload, 'SEED', { expiresIn: duration }, (err, token) => {
+            jwt.sign(payload, JWT_SEED, { expiresIn: duration }, (err, token) => {
                 
                 if ( err ) return resolve(null);
                 
@@ -23,7 +24,7 @@ export class JtwAdappter {
     static validaTeToken<T>(token: string): Promise<T | null> {
 
         return new Promise ( ( resolve ) => {
-            jwt.verify( token, 'SEED', (err, decoded) => {
+            jwt.verify( token, JWT_SEED, (err, decoded) => {
                 
                 if ( err ) return resolve(null);
                 
